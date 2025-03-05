210838eab7c30e4c3bef05145abf82737e7407a7
Yang Fu <yang.fu3785@gmail.com>
2025-03-06 00:40:23 +0100

    Initial commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from '../Header'
+import Landing from '../Landing'
+import '/src/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Header/>
+       <Landing/>
+      </div>
+    </>
+  );
+}
+
+export default App

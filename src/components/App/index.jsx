a545879fd99beb604740f34b2aa7bb636cc9d101
Yang Fu <yang.fu3785@gmail.com>
2025-06-02 15:52:30 +0200

    component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+
 import Header from '../Header'
 import Landing from '../Landing'
 import Footer from '../Footer'
+import Welcome from '../Welcome'
+import Login from '../Login'
+import Signup from '../Signup'
+import ErrorPage from '../ErrorPage'
 import '/src/App.css'
 
 function App() {
@@ -10,7 +14,11 @@ function App() {
     <>
       <div>
        <Header/>
+       <Welcome/>
        <Landing/>
+       <Login/>
+       <Signup/>
+       <ErrorPage/>
        <Footer/>
       </div>
     </>
---
+++ src/components/ErrorPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='quizz-bg'>
+        <div className='container'>
+            ErrorPage
+        </div>
+
+    </div>
+  )
+}
+
+export default ErrorPage

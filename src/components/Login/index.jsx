d0b06aadf83a6dd2e7a7f7247e7313793f191f35
Yang Fu <yang.fu3785@gmail.com>
2025-06-16 21:16:00 +0200

    login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,30 +1,58 @@
-import {useState} from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseContext } from '../Firebase';
 
+const Login = (props) => {
 
-const Login = () => {
+ const firebase = useContext(FirebaseContext);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [btn, setBtn] = useState(false);
+  const [error, setError] = useState('')
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    if (password.length > 5 && email !== ""){
+        setBtn(true)
+    }else if(btn) {
+         setBtn(false)
+    }
+  },[password, email, btn])
 
+  const handlePassword = e => {
+    setPassword(e.target.value);
+  }
 
-const handlePassword = e => {
-  setPassword(e.target.value);
-}
-
+  const handleSubmit = e => {
+    e.preventDefault();
+    firebase.loginUser(email, password)
+    .then(user =>{
+      console.log(user)
+      setEmail('');
+      setPassword('');
+       navigate('/welcome');
+    })
+    .catch(error => {
+      setError(error);
+      setEmail('');
+      setPassword('');
+    })
+  }
 
   return (
     <div className='signUpLoginBox'>
-        <div className='slContainer'>
-              <div className='formBoxLeftLogin'>
+      <div className='slContainer'>
+        <div className='formBoxLeftLogin'>
 
         </div>
         <div className='formBoxRight'>
-          <form >
+          <form onSubmit={handleSubmit}>
+            {error !== '' && <span>{error.message}</span>}
             <h2>Connexion</h2>
 
             <div className='inputBox'>
-              <input onChange={e =>  setEmail(e.target.value)} value={email} type='email' id='email' required />
+              <input onChange={e => setEmail(e.target.value)} value={email} type='email' id='email' required />
               <label htmlFor='email'>Email</label>
             </div>
             <div className='inputBox'>
@@ -35,9 +63,10 @@ const handlePassword = e => {
             <div className='linkContainer'>
               <Link className='simpleLink' to="/signup">Nouveau sur marvel Quiz ? Inscrivez-vous maintenant</Link>
             </div>
+            {btn ? <button>Connexion</button> : <button disabled>Connexion</button> }
           </form>
         </div>
-        </div>
+      </div>
     </div>
   )
 }

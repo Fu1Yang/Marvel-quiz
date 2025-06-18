c57877d5c0c63dc1a61644d9c07635fd8f7c7d2f
Yang Fu <yang.fu3785@gmail.com>
2025-06-18 18:44:24 +0200

    rajout du logout

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,16 +1,22 @@
 import  {useState, useEffect, useContext} from 'react'
 import { FirebaseContext } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
     const firebase = useContext(FirebaseContext)
     const [checked, setChecked] = useState(false);   
-
+    const navigate = useNavigate();
     
 
     useEffect(()=>{
         if(checked){
             console.log("Déconnexion");
             firebase.signoutUser();
+            let end = setTimeout(()=>{
+                navigate('/')
+            },1000);
+            
+            return () => clearTimeout(end);
         }
     },[checked, firebase])
 

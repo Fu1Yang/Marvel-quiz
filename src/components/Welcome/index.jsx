bd0176f265079fd14b43971e14c62e04b3112a55
Yang Fu <yang.fu3785@gmail.com>
2025-06-18 19:07:33 +0200

    gestion user

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useState, useContext, useEffect} from 'react'
 import Logout from '../Logout'
 import Quiz from '../Quiz'
+import { FirebaseContext } from '../Firebase'
+import { useNavigate } from 'react-router-dom';
+
 
 const Welcome = () => {
-  return (
+  const firebase = useContext(FirebaseContext);
+  const [userSession, setUserSession] = useState(null);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    let listener = firebase.auth.onAuthStateChanged(user => {
+      user ? setUserSession(user) :  navigate('/')
+    })
+
+    return ()=>{
+      listener()
+    };
+  },[])
+
+  return  userSession === null ? (
+   <>
+      <div className='loader'></div>
+      <p className='loaderText'>Loading ...</p>
+   </>
+  ) : (
+
     <div className='quiz-bg'>
         <div className='container'>
            <Logout/>
@@ -11,6 +33,10 @@ const Welcome = () => {
         </div>
     </div>
   )
+
+
+
+
 }
 
 export default Welcome
